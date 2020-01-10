77e2a10bb8c4fc48f0bb443828ff9d746278c843
wilfreddenton <dentonw3@gmail.com>
2020-01-10 18:55:14 +0000

    cleaner implementation

--- chapter-two/src/Main.hs
+++ chapter-two/src/Main.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Lens
+import Control.Monad ((<=<))
 import Data.List
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NE
@@ -14,25 +17,21 @@ type Column a = [a]
 
 column :: (Read a) => String -> CSV' -> Either String (Column a)
 column name rows = do
-  i <-
-    maybe (Left ("Column " <> name <> " not found.")) return
-      . findIndex (name ==)
-      $ NE.head rows
-  return . catMaybes . fmap ((=<<) readMaybe . (^? element i)) $ NE.tail rows
+  i <- maybe err return . findIndex (name ==) $ NE.head rows
+  return . catMaybes . fmap (readMaybe <=< (^? element i)) $ NE.tail rows
+  where
+    err = Left ("Column " <> name <> " not found.")
 
 columnAverage :: Column Double -> Double
 columnAverage = (/) <$> sum <*> genericLength
 
 applyToColumn :: (Read a) => (Column a -> b) -> String -> CSV' -> Either String b
-applyToColumn f name = (=<<) (return . f) . column name
+applyToColumn f name = fmap f . column name
 
-applyToCSVColumn :: (Read a) => (Column a -> b) -> String -> FilePath -> IO (Either String b)
-applyToCSVColumn f name path = do
-  eRows <- parseCSVFromFile path
-  return $
-    applyToColumn f name
-      =<< maybe (Left "CSV is empty.") return . NE.nonEmpty
-      =<< either (Left . show) return eRows
+applyToCSVColumn :: (Read a) => (Column a -> b) -> String -> CSV -> Either String b
+applyToCSVColumn f name = applyToColumn f name <=< maybe (Left "CSV is empty.") return . NE.nonEmpty
 
 main :: IO ()
-main = either error print =<< applyToCSVColumn columnAverage "mag" "all_week.csv"
+main = do
+  eRows <- either (Left . show) return <$> parseCSVFromFile "all_week.csv"
+  either error print (applyToCSVColumn columnAverage "mag" =<< eRows)
